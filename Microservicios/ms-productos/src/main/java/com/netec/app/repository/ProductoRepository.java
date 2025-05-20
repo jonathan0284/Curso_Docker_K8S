afedc49a5deb5efe8ecf34f816f38286856cb346
Jonathan Ramirez Vasquez <jonathan.0284@gmail.com>
2025-05-20 00:36:48 -0500

    Update Proyecto Final

--- Microservicios/ms-productos/src/main/java/com/netec/app/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.netec.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.netec.app.entities.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    boolean existsByNombre(String nombre);
-}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Curso_Docker_K8S
 Proyecto Curso Docker y K8S Intermedio
+ye
